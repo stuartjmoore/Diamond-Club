f3e2479543349f5fed02c159a1aa8b02441839d0
stuartjmoore <stuartjmoore@gmail.com>
2016-12-10 15:36:52 -0500

    Initial Commit

---
+++ Diamond Club/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Diamond Club
+//
+//  Created by Stuart Moore on 12/10/16.
+//  Copyright © 2016 Stuart J. Moore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

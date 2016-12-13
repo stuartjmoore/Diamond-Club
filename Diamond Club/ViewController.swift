f64cdaf118112856ed5a30a29eb1f7400448fd5b
stuartjmoore <stuartjmoore@gmail.com>
2016-12-12 20:59:39 -0500

    Converted to swipe channel changing but I don’t like it. Re-thinking.

--- Diamond Club/ViewController.swift
+++ Diamond Club/ViewController.swift
@@ -17,10 +17,7 @@ class ViewController: UIViewController {
         case left, right
     }
 
-    fileprivate let leftTapGesture = UITapGestureRecognizer()
-    fileprivate let rightTapGesture = UITapGestureRecognizer()
-
-    private var playerItem: AVPlayerItem? {
+    fileprivate var playerItem: AVPlayerItem? {
         willSet {
             playerItem?.removeObserver(self, forKeyPath: "status", context: &AVPlayerItemStatusObservationContext)
         } didSet {
@@ -28,8 +25,18 @@ class ViewController: UIViewController {
         }
     }
 
-    private var player: AVPlayer?
-    private var playerViewController: AVPlayerViewController?
+    fileprivate var player: AVPlayer?
+    fileprivate var playerViewController: AVPlayerViewController?
+
+    @IBOutlet private var changeChannelView: UIView!
+    @IBOutlet private var changeChannelPrevConstraint: NSLayoutConstraint!
+    @IBOutlet private var changeChannelNextConstraint: NSLayoutConstraint!
+
+    @IBOutlet private var currentChannelImageView: UIImageView!
+    @IBOutlet private var currentChannelLabel: UILabel!
+
+    @IBOutlet private var prevChannelLabel: UILabel!
+    @IBOutlet private var nextChannelLabel: UILabel!
 
     private var topConstraint: NSLayoutConstraint?
     private var botomConstraint: NSLayoutConstraint?
@@ -38,6 +45,8 @@ class ViewController: UIViewController {
     private var rightConstraint: NSLayoutConstraint?
     private var offRightContraint: NSLayoutConstraint?
     private var offLeftContraint: NSLayoutConstraint?
+
+    fileprivate let panGesture = UIPanGestureRecognizer()
 
     private let chatRealm = IRCClient(host: "irc.chatrealm.net", port: 6667, room: "chat", nick: "AppleTV\(arc4random_uniform(10_000))")
 
@@ -51,16 +60,14 @@ class ViewController: UIViewController {
     // MARK: -
 
     override func viewDidLoad() {
-        leftTapGesture.addTarget(self, action: #selector(handleLeftTapGesture(_:)))
-        leftTapGesture.allowedPressTypes = [NSNumber(value: UIPressType.leftArrow.rawValue as Int)]
-        leftTapGesture.delegate = self
-
-        rightTapGesture.addTarget(self, action: #selector(handleRightTapGesture(_:)))
-        rightTapGesture.allowedPressTypes = [NSNumber(value: UIPressType.rightArrow.rawValue as Int)]
-        rightTapGesture.delegate = self
-
 //        chatRealm.delegate = self
 //        chatRealm.start()
+
+        changeChannelView.alpha = 0
+
+        panGesture.addTarget(self, action: #selector(handlePanGesture(_:)))
+        panGesture.delegate = self
+        view.addGestureRecognizer(panGesture)
 
         DiamondClub.getLiveChannels() { (channels) in
             self.channels = channels
@@ -73,55 +80,99 @@ class ViewController: UIViewController {
         }
     }
 
-    // MARK: - Gesture Recognizers
+    // MARK: - Overlay
 
-    func handleLeftTapGesture(_ gesture: UITapGestureRecognizer) {
-        guard let channelIndex = currentChannelIndex, let channels = channels, channels.count > 1 else {
-            return bouncePlayer(from: .left)
-        }
-
-        let prevChannelIndex = (channelIndex == 0) ? channels.count - 1 : channelIndex - 1
-        let url = DiamondClub.streamURL(for: channels[prevChannelIndex].number)
-
-        addPlayerViewController(playing: url, from: .left)
-        currentChannelIndex = prevChannelIndex
+    private func showChangeChannelView() {
+        UIViewPropertyAnimator(duration: 0.1, curve: .easeIn, animations: {
+            self.changeChannelView.alpha = 1
+        }).startAnimation()
     }
 
-    func handleRightTapGesture(_ gesture: UITapGestureRecognizer) {
-        guard let channelIndex = currentChannelIndex, let channels = channels, channels.count > 1 else {
-            return bouncePlayer(from: .right)
+    private func hideChangeChannelView() {
+        changeChannelPrevConstraint.constant = 90
+        changeChannelNextConstraint.constant = 90
+
+        UIViewPropertyAnimator(duration: 1, curve: .easeOut, animations: {
+            self.changeChannelView.alpha = 0
+            self.changeChannelView.layoutIfNeeded()
+        }).startAnimation()
+
+        leftConstraint?.constant = 0
+        rightConstraint?.constant = 0
+
+        UIViewPropertyAnimator(duration: 0.75, dampingRatio: 0.25, animations: {
+            self.view.layoutIfNeeded()
+        }).startAnimation()
+    }
+
+    // MARK: - Touches
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        showChangeChannelView()
+        super.touchesBegan(touches, with: event)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hideChangeChannelView()
+        super.touchesEnded(touches, with: event)
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        hideChangeChannelView()
+        super.touchesCancelled(touches, with: event)
+    }
+
+    func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .possible:
+            break
+
+        case .began:
+            showChangeChannelView()
+
+        case .changed:
+            showChangeChannelView()
+
+            let translation = gesture.translation(in: gesture.view).x / 6
+
+            if translation > 0 {
+                leftConstraint?.constant = abs(translation)
+                rightConstraint?.constant = -abs(translation)
+                changeChannelPrevConstraint.constant = 90 + abs(translation) / 2
+                changeChannelNextConstraint.constant = 90
+            } else if translation < 0 {
+                leftConstraint?.constant = -abs(translation)
+                rightConstraint?.constant = abs(translation)
+                changeChannelPrevConstraint.constant = 90
+                changeChannelNextConstraint.constant = 90 + abs(translation) / 2
+            }
+
+        case .ended:
+            let translation = gesture.translation(in: gesture.view).x
+            let boundsWidth = gesture.view?.frame.width ?? 0
+
+            if let channelIndex = currentChannelIndex, let channels = channels, channels.count > 1 {
+                if translation > 0 && translation > boundsWidth / 2 {
+                    let prevChannelIndex = (channelIndex == 0) ? channels.count - 1 : channelIndex - 1
+                    let url = DiamondClub.streamURL(for: channels[prevChannelIndex].number)
+                    currentChannelIndex = prevChannelIndex
+                    addPlayerViewController(playing: url, from: .left)
+                } else if translation < 0  && -translation > boundsWidth / 2 {
+                    let nextChannelIndex = (channelIndex == channels.count - 1) ? 0 : channelIndex + 1
+                    let url = DiamondClub.streamURL(for: channels[nextChannelIndex].number)
+                    currentChannelIndex = nextChannelIndex
+                    addPlayerViewController(playing: url, from: .right)
+                }
+            }
+
+            fallthrough
+
+        case .failed, .cancelled:
+            hideChangeChannelView()
         }
-
-        let nextChannelIndex = (channelIndex == channels.count - 1) ? 0 : channelIndex + 1
-        let url = DiamondClub.streamURL(for: channels[nextChannelIndex].number)
-
-        addPlayerViewController(playing: url, from: .right)
-        currentChannelIndex = nextChannelIndex
     }
 
     // MARK: - Change Channel Streams
-
-    private func bouncePlayer(from direction: Direction) {
-        self.leftConstraint?.constant = (direction == .left) ? 90 : 0
-        self.rightConstraint?.constant = (direction == .right) ? 90 : 0
-
-        let slideOutAnimator = UIViewPropertyAnimator(duration: 0.15, curve: .linear) {
-            self.view.layoutIfNeeded()
-        }
-
-        slideOutAnimator.addCompletion() { _ in
-            self.leftConstraint?.constant = 0
-            self.rightConstraint?.constant = 0
-
-            let bounceInAnimator = UIViewPropertyAnimator(duration: 0.75, dampingRatio: 0.25) {
-                self.view.layoutIfNeeded()
-            }
-            
-            bounceInAnimator.startAnimation()
-        }
-
-        slideOutAnimator.startAnimation()
-    }
 
     private func addPlayerViewController(playing url: URL, from direction: Direction? = nil) {
         let playerItem = AVPlayerItem(url: url)
@@ -130,15 +181,12 @@ class ViewController: UIViewController {
         let player = AVPlayer(playerItem: playerItem)
 
         let playerViewController = AVPlayerViewController()
-        playerViewController.player = player
         playerViewController.view.frame = view.bounds
         playerViewController.view.alpha = (direction == nil) ? 1 : 0
-        playerViewController.view.addGestureRecognizer(leftTapGesture)
-        playerViewController.view.addGestureRecognizer(rightTapGesture)
 
         addChildViewController(playerViewController)
         playerViewController.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(playerViewController.view)
+        view.insertSubview(playerViewController.view, at: 0)
         playerViewController.didMove(toParentViewController: self)
 
         let topConstraint = playerViewController.view.topAnchor.constraint(equalTo: view.topAnchor)
@@ -199,9 +247,36 @@ class ViewController: UIViewController {
             self.rightConstraint = rightConstraint
             self.offRightContraint = offRightContraint
             self.offLeftContraint = offLeftContraint
+
+            playerViewController.player = player
         }
         
         slideInAnimator.startAnimation()
+
+        if let channelIndex = currentChannelIndex, let channels = channels, channelIndex < channels.count {
+            DiamondClub.getChannelIcon(for: channels[channelIndex].number) { (image) in
+                self.currentChannelImageView.image = image
+            }
+
+            currentChannelLabel.text = channels[channelIndex].title
+
+            if channelIndex - 1 >= 0 {
+                prevChannelLabel.text = channels[channelIndex - 1].title
+            } else if channels.count > 0 {
+                prevChannelLabel.text = channels[channels.count - 1].title
+            } else {
+                prevChannelLabel.text = nil
+            }
+
+            if channelIndex + 1 < channels.count {
+                nextChannelLabel.text = channels[channelIndex + 1].title
+            } else if channels.count > 0 {
+                nextChannelLabel.text = channels[0].title
+            } else {
+                nextChannelLabel.text = nil
+            }
+
+        }
     }
 
     // MARK: - KVO
@@ -227,6 +302,14 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UIGestureRecognizerDelegate {
+
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return gestureRecognizer === panGesture && player?.rate != 0
+    }
+
+}
+
 extension ViewController: IRCClientDelegate {
 
     func IRC(_ client: IRCClient, didReceiveCommand command: IRCClient.Command, withMessage message: String?, andArguments: [String], fromSource: String?) {
@@ -243,14 +326,6 @@ extension ViewController: IRCClientDelegate {
 
     func IRCStreamDidEnd(_ client: IRCClient) {
         return
-    }
-
-}
-
-extension ViewController: UIGestureRecognizerDelegate {
-
-    func gestureRecognizer(_ gesture: UIGestureRecognizer, shouldBeRequiredToFailBy otherGesture: UIGestureRecognizer) -> Bool {
-        return (gesture === leftTapGesture && gesture.view === otherGesture.view) || (gesture === rightTapGesture && gesture.view === otherGesture.view)
     }
 
 }

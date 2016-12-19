a403c06efb6a4b8586001851908d4728f48622c2
stuartjmoore <stuartjmoore@gmail.com>
2016-12-19 17:36:04 -0500

    Create Schedule Kit.

---
+++ Schedule Kit/Schedule Kit/Schedule Kit.h
@@ -0,0 +1,19 @@
+//
+//  Schedule Kit.h
+//  Schedule Kit
+//
+//  Created by Stuart Moore on 12/19/16.
+//  Copyright © 2016 Stuart J. Moore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Schedule Kit.
+FOUNDATION_EXPORT double Schedule_KitVersionNumber;
+
+//! Project version string for Schedule Kit.
+FOUNDATION_EXPORT const unsigned char Schedule_KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Schedule_Kit/PublicHeader.h>
+
+

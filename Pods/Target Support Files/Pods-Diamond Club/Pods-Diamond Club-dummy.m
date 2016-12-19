0447e5c7a5eefae6f9adda63c0ff5ca6e80756d1
stuartjmoore <stuartjmoore@gmail.com>
2016-12-19 17:34:06 -0500

    pod init

---
+++ Pods/Target Support Files/Pods-Diamond Club/Pods-Diamond Club-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Diamond_Club : NSObject
+@end
+@implementation PodsDummy_Pods_Diamond_Club
+@end
---
+++ Pods/Target Support Files/Pods-Diamond Club/Pods-Diamond Club-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Diamond_ClubVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Diamond_ClubVersionString[];
+
